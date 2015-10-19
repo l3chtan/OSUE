f1388785d78bb80110d4f28107ca30220e86e91c
Edward Toth <e0725631@student.tuwien.ac.at>
2015-10-19 20:45:20 +0200

    restore point

--- Task1A/mycompress.c
+++ Task1A/mycompress.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_LINE_LEN 80
+
 char* append(char* source, char c){
 	int len = strlen(source);
 	char * dest = (char*)malloc(len+2);
@@ -37,14 +39,14 @@ int read_input(FILE *stream, char *original_name){
 
 	int cnt_original = 0;
 	int cnt_comp = 0;
-	char *buf = (char *) malloc(81);
-	char *updated = malloc(81);
+	char *buf = (char *) malloc(MAX_LINE_LEN+1);
+	char *updated = malloc(MAX_LINE_LEN+1);
 	char *new_buf = (char *) malloc(1);
 	FILE *output;
 
 
-	fgets(updated,81,stream);
-	while(fgets(buf,81,stream)){
+	fgets(updated,MAX_LINE_LEN+1,stream);
+	while(fgets(buf,MAX_LINE_LEN+1,stream)){
 
 		char *tmp = malloc(strlen(buf)+strlen(updated));
 
@@ -65,31 +67,16 @@ int read_input(FILE *stream, char *original_name){
 
 	fputs(new_buf,output);
 	fclose(output);
+
+	printf("%s:\t\t%d Zeichen\n",original_name,(int) strlen(updated));
+	printf("%s.comp: \t%d Zeichen\n",original_name,(int) strlen(new_buf));	
+
+	free(updated);
 	free(new_name);
 	free(new_buf);
 	free(buf);
-	printf("%s:\t\t%d Zeichen\n",original_name,cnt_original);
-	printf("%s.comp: %d Zeichen\n",original_name,cnt_comp);	
 	return 0;
 }
-
-int files_input(int argc, char **argv){
-
-	int i = 0;
-	FILE *input;
-
-	for(i = 1; i<argc;++i){
-		input = fopen(argv[i],"r");
-
-		if(read_input(input,argv[i]) != 0){
-			return 1;
-		}
-		fclose(input);
-	}
-
-	return 0;
-}
-
 
 int main(int argc, char **argv){
 
@@ -100,8 +87,16 @@ int main(int argc, char **argv){
 			}
 			break;
 		default: 
-			if(files_input(argc, argv) != 0){
-				return 1;
+			int i = 0;
+			FILE *input;
+
+			for(i = 1; i<argc;++i){
+				input = fopen(argv[i],"r");
+
+				if(read_input(input,argv[i]) != 0){
+					return 1;
+				}
+				fclose(input);
 			}
 			break;
 	}
