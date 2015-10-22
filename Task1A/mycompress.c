0548c09908c61400a8e46dc3a1e76f254e8415b2
Edward Toth <e0725631@student.tuwien.ac.at>
2015-10-22 18:32:17 +0200

    working version - ugly

--- Task1A/mycompress.c
+++ Task1A/mycompress.c
@@ -2,62 +2,80 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_LINE_LEN 80
+#define MAX_LINE_LEN  80
 
 char* append(char* source, char c){
+	char * tmp = realloc(source,strlen(source)+2);
+	if(tmp == NULL) return NULL;
+	source = tmp;
 	int len = strlen(source);
-	char * dest = (char*)malloc(len+2);
-	strcpy(dest,source);
-	dest[len] = c;
-	dest[len+1] = '\0';
-	free(source);
-	return dest;
+	source[len] = c;
+	source[len+1] = '\0';
+	return source;
 }
 
 char* compress(char *line, char* buf){
-	int i = 0, cnt = 0;
+	char * dest = (char*)malloc(1);
+	int i = 0, cnt = 0, ret = 0;
 	char c = line[0];
 
-	buf = append(buf,c);
+	dest = append(dest,c);
+	// if(ret != 0) return ret;
 	while(line[i] != '\0'){
 		if(line[i] == c){
 			cnt++;
 		} else {
-			buf = append(buf,cnt + '0');
-			buf = append(buf,line[i]);
+			char *nums = (char*)malloc(10);
+			int j = 0;
+			while(cnt > 0){
+				nums[j] = cnt % 10;
+				nums[j] = nums[j] +48;
+				cnt = cnt/10;
+				j++;
+			}
+			nums[j] = '\0';
+			char * t = realloc(dest,strlen(dest)+strlen(nums));
+			if(t == NULL) return NULL;
+			dest = t;
+			strcat(dest,nums);
+			free(nums);
+			// dest = append(dest,cnt + '0');
+			// if(ret != 0) return ret;
+			dest = append(dest,line[i]);
+			// if(ret != 0) return ret;
 			cnt = 1;
 		c = line[i];
 		}
 		i++;
 	}
-	buf = append(buf,cnt+'0');
-	buf = append(buf,c);
-	return buf;
+	dest = append(dest,cnt+'0');
+	// if(ret != 0) return ret;
+	dest = append(dest,c);
+	// if(ret != 0) return ret;
+	return dest;
 }
 
 int read_input(FILE *stream, char *original_name){
+	int ret = 0;
 
-	int cnt_original = 0;
-	int cnt_comp = 0;
 	char *buf = (char *) malloc(MAX_LINE_LEN+1);
-	char *updated = malloc(MAX_LINE_LEN+1);
+	char *updated = (char *) malloc(1);
 	char *new_buf = (char *) malloc(1);
 	FILE *output;
 
 
-	fgets(updated,MAX_LINE_LEN+1,stream);
 	while(fgets(buf,MAX_LINE_LEN+1,stream)){
-
-		char *tmp = malloc(strlen(buf)+strlen(updated));
-
-		strcpy(tmp,updated);
-		strcat(tmp,buf);
-		free(updated);
+		char * tmp = realloc(updated,strlen(updated)+strlen(buf)+1);
+		if(tmp == NULL){
+			return -1;
+		}
 		updated = tmp;
+		strcat(updated,buf);
 
 		if(buf[0] == EOF) break;
 	}
-		new_buf = compress(updated,new_buf);
+	new_buf = compress(updated,new_buf);
+	// if(ret != 0) return ret;
 
 	char *new_name = (char*) malloc(strlen(original_name)+5);
 	strcpy(new_name, original_name);
@@ -79,17 +97,15 @@ int read_input(FILE *stream, char *original_name){
 }
 
 int main(int argc, char **argv){
-
-	switch(argc){
-		case 1: 
-			if(read_input(stdin, "Stdin") != 0){
-				return 1;
-			}
-			break;
-		default: 
 			int i = 0;
 			FILE *input;
 
+	switch(argc){
+		case 1: 
+			printf("To end the input press Ctrl-D\n");
+			return read_input(stdin, "Stdin");
+
+		default: 
 			for(i = 1; i<argc;++i){
 				input = fopen(argv[i],"r");
 
@@ -100,6 +116,5 @@ int main(int argc, char **argv){
 			}
 			break;
 	}
-
 	return 0;
 }
